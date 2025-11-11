0ecc6a0dbfc28e1e8644d68701cd8a8c91ac9379
Afiqu3 <gahossainj@gmail.com>
2025-11-11 23:15:56 +0600

    Adjust Navbar for user

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const { theme, setTheme } = useTheme();
-    const { user, signOutUser } = useAuth();
+  const { user, signOutUser } = useAuth();
   const navigate = useNavigate();
 
   const links = navigationData.map((nav) => (
@@ -160,96 +160,232 @@ const Navbar = () => {
           </motion.div>
 
           <div className="lg:flex gap-x-4 items-center hidden">
-            <ul className="lg:flex hidden gap-x-8 font-medium">{links} {user && privateLinks}</ul>
+            <ul className="lg:flex hidden gap-x-8 font-medium">
+              {links} {user && privateLinks}
+            </ul>
           </div>
 
           <div className="lg:flex gap-x-4 items-center hidden">
             {/* <ul className="lg:flex hidden gap-x-8 font-medium">{links}</ul> */}
-            <motion.div
-              initial={{ opacity: 0, y: -15 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, ease: "easeOut" }}
-            >
-              <div className="flex items-center gap-x-2">
-                <Link
-                  to={"/login"}
-                  target="_parent"
-                  className="hidden group lg:flex items-center gap-2 my-btn rounded-full text-black"
-                >
-                  <FaUser />
-                  <span>Login</span>
-                </Link>
-                <Link
-                  to={"/register"}
-                  target="_parent"
-                  className="hidden group lg:flex items-center gap-2 my-btn rounded-full text-black"
-                >
-                  <FaUserPlus />
-                  <span>Register</span>
-                </Link>
+            {!user && (
+              <motion.div
+                initial={{ opacity: 0, y: -15 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+              >
+                <div className="flex items-center gap-x-2">
+                  <Link
+                    to={"/login"}
+                    target="_parent"
+                    className="hidden group lg:flex items-center gap-2 my-btn rounded-full text-black"
+                  >
+                    <FaUser />
+                    <span>Login</span>
+                  </Link>
+                  <Link
+                    to={"/register"}
+                    target="_parent"
+                    className="hidden group lg:flex items-center gap-2 my-btn rounded-full text-black"
+                  >
+                    <FaUserPlus />
+                    <span>Register</span>
+                  </Link>
 
-                <div>
-                  <label className="swap swap-rotate">
-                    {/* this hidden checkbox controls the state */}
-                    <input
-                      onChange={(e) => handleTheme(e.target.checked)}
-                      type="checkbox"
-                      className="theme-controller"
-                      defaultChecked={localStorage.getItem("theme") === "dark"}
-                    />
+                  <div>
+                    <label className="swap swap-rotate">
+                      {/* this hidden checkbox controls the state */}
+                      <input
+                        onChange={(e) => handleTheme(e.target.checked)}
+                        type="checkbox"
+                        className="theme-controller"
+                        defaultChecked={
+                          localStorage.getItem("theme") === "dark"
+                        }
+                      />
 
-                    {/* sun icon */}
-                    <svg
-                      className="swap-off h-10 w-10 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                    </svg>
+                      {/* sun icon */}
+                      <svg
+                        className="swap-off h-10 w-10 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
 
-                    {/* moon icon */}
-                    <svg
-                      className="swap-on h-10 w-10 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                    </svg>
-                  </label>
+                      {/* moon icon */}
+                      <svg
+                        className="swap-on h-10 w-10 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    </label>
+                  </div>
                 </div>
+              </motion.div>
+            )}
+
+            {/* Big screen and user is true */}
+            {user && (
+              <div className="">
+                <motion.div
+                  className="dropdown dropdown-hover dropdown-center"
+                  initial={{ opacity: 0, y: -15 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, ease: "easeOut" }}
+                >
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    className="cursor-pointer m-1"
+                  >
+                    <img src={user.photoURL} alt="" />
+                  </div>
+                  <ul
+                    tabIndex="-1"
+                    className="dropdown-content menu bg-[#244034] text-white rounded-box z-1 w-52 p-2 shadow-sm"
+                  >
+                    <li className="pointer-events-none cursor-not-allowed text-gray-400">
+                      <p>{user.displayName}</p>
+                    </li>
+                    <li className="flex justify-center hover:bg-[#D2F34C] hover:text-black">
+                      <div className="flex items-center">
+                        <label className="swap swap-rotate">
+                          {/* this hidden checkbox controls the state */}
+                          <input
+                            onChange={(e) => handleTheme(e.target.checked)}
+                            type="checkbox"
+                            className="theme-controller"
+                            defaultChecked={
+                              localStorage.getItem("theme") === "dark"
+                            }
+                          />
+
+                          {/* sun icon */}
+                          <svg
+                            className="swap-off h-8 w-8 fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                          </svg>
+
+                          {/* moon icon */}
+                          <svg
+                            className="swap-on h-8 w-8 fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                          </svg>
+                        </label>
+                      </div>
+                    </li>
+                    <li className="hover:bg-[#D2F34C] hover:text-black">
+                      <Link onClick={handleLogOut}>
+                        <FiLogOut></FiLogOut>Logout
+                      </Link>
+                    </li>
+                  </ul>
+                </motion.div>
               </div>
-            </motion.div>
+            )}
           </div>
 
-          <div className="lg:hidden block">
-            <label className="swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input
-                onChange={(e) => handleTheme(e.target.checked)}
-                type="checkbox"
-                className="theme-controller"
-                defaultChecked={localStorage.getItem("theme") === "dark"}
-              />
+          {!user && (
+            <div className="lg:hidden block">
+              <label className="swap swap-rotate">
+                {/* this hidden checkbox controls the state */}
+                <input
+                  onChange={(e) => handleTheme(e.target.checked)}
+                  type="checkbox"
+                  className="theme-controller"
+                  defaultChecked={localStorage.getItem("theme") === "dark"}
+                />
 
-              {/* sun icon */}
-              <svg
-                className="swap-off h-10 w-10 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-              </svg>
+                {/* sun icon */}
+                <svg
+                  className="swap-off h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
 
-              {/* moon icon */}
-              <svg
-                className="swap-on h-10 w-10 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+                {/* moon icon */}
+                <svg
+                  className="swap-on h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+              </label>
+            </div>
+          )}
+          
+          {/* small or medium screen and user is true */}
+          {user && (
+            <div className="lg:hidden block">
+              <motion.div
+                className="dropdown dropdown-bottom dropdown-end"
+                initial={{ opacity: 0, y: -15 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, ease: "easeOut" }}
               >
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-              </svg>
-            </label>
-          </div>
+                <div tabIndex={0} role="button" className="cursor-pointer m-1">
+                  <img src={user.photoURL} alt="" />
+                </div>
+                <ul
+                  tabIndex="-1"
+                  className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+                >
+                  <li>
+                    <p>{user.displayName}</p>
+                  </li>
+                  <li className="flex justify-center">
+                    <div className="flex items-center">
+                      <label className="swap swap-rotate">
+                        {/* this hidden checkbox controls the state */}
+                        <input
+                          onChange={(e) => handleTheme(e.target.checked)}
+                          type="checkbox"
+                          className="theme-controller"
+                          defaultChecked={
+                            localStorage.getItem("theme") === "dark"
+                          }
+                        />
+
+                        {/* sun icon */}
+                        <svg
+                          className="swap-off h-8 w-8 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                        </svg>
+
+                        {/* moon icon */}
+                        <svg
+                          className="swap-on h-8 w-8 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                        </svg>
+                      </label>
+                    </div>
+                  </li>
+                  <li>
+                    <Link onClick={handleLogOut}>
+                      <FiLogOut></FiLogOut>Logout
+                    </Link>
+                  </li>
+                </ul>
+              </motion.div>
+            </div>
+          )}
 
           {/* {user && (
           <div className="flex gap-6 items-center">
@@ -287,7 +423,6 @@ const Navbar = () => {
         )} */}
         </nav>
 
-
         {/*  mobile and tab */}
         {isOpen && !user && (
           <div className="lg:hidden mt-2 mb-5 ml-10 space-y-3 max-w-45 relative h-45">
@@ -319,17 +454,12 @@ const Navbar = () => {
           </div>
         )}
         {isOpen && user && (
-        <div className="lg:hidden mt-2 mb-5 ml-10 space-y-3 max-w-45 relative h-55">
-          <ul className={`space-y-3 animation`}>{links} {privateLinks}</ul>
-          <button
-            onClick={handleLogOut}
-            className="animation lg:hidden group flex items-center gap-2 bg-linear-to-r from-[#52057B] to-[#892CDC] text-white font-semibold px-6 py-2 rounded-full shadow-md transition-all duration-300 ease-in-out transform focus:ring-2 focus:ring-[#892CDC] focus:outline-none cursor-pointer"
-          >
-            <FiLogOut className="text-white text-lg transition-transform duration-300 group-hover:-translate-x-1" />
-            <span>Logout</span>
-          </button>
-        </div>
-      )}
+          <div className="lg:hidden mt-2 mb-5 ml-10 space-y-3 max-w-45 relative h-55">
+            <ul className={`space-y-3 animation`}>
+              {links} {privateLinks}
+            </ul>
+          </div>
+        )}
       </div>
     </section>
   );
